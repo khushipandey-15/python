25e7b98bf4c46512e25f2b0e2ba1199d86cc35e9
khushipandey-15 <153102876+khushipandey-15@users.noreply.github.com>
2024-02-06 21:22:44 +0530

    Create basic_python.py

---
+++ basic_python.py
@@ -0,0 +1,69 @@
+#DAY 10
+# X=int(input("enter the value:"))
+# y=int(input("enter y value:"))
+# print("addition:",X+y)
+# print("subtraction:",X-y)
+# print("multiple:",X*y)
+# print("precent:",X%y)
+# print("divide:",X/y)
+#DAY 11
+# a="chural"
+# for character in a:
+#  print(character)
+#DAY 12
+# name="harry"
+# print(name[0:9:3])
+# print(name[:10])
+# print(name[1:14])
+# print(name[:-1])
+# print(name[-4:-2])
+
+#DAY 13
+
+# a="vaibhav33"
+# print(a.upper())
+# print(a.lower())
+# print(a.title())
+# print(a.rstrip('3'))
+# print(a.replace('vaibhav','khushi'))
+# print(a.lstrip('v'))
+
+# str="everything!!! is awosome"
+# print(str.endswith("!!!"))
+# print(str.endswith("!!",6,13))
+# print(str.startswith("nve"))
+# print(str.startswith("eve"))
+# print(str.split(" "))
+# print(str.capitalize())
+# #print(str.find("isa"))
+# #print(str.index("!!is"))
+# print(str.find("is"))
+# print(str.index("awo"))
+# a="charbottewotka"
+# print(a.isalnum())
+# print(a.isalpha())
+# print(a.isprintable())
+# a="    "
+# print(a.isspace())
+#DAY 14
+
+# a=int(input("enter you age:"))
+# if a<18:
+#     print("you can drive")
+# elif a==18:
+#     print("you have to make you driving licence")   
+# elif a>18:
+#     print("you can not drive")     
+
+# DAY 15
+# a=int(input("the time is" ))
+# if (a>=6 and a<=12) :
+#     print("good morning re baba")
+# elif  (a>12 and a<=16):
+#     print("good afternoon")
+# elif (a>16 and a<=20):
+#     print("good evening")
+# elif(a>20 and a<=24):
+#     print("good night")    
+# else:
+#     print("vaibhav mota hai")          
