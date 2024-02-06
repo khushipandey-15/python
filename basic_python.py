ec710ef88ae923efb5b77f1611e74f352e1218fd
khushipandey-15 <153102876+khushipandey-15@users.noreply.github.com>
2024-02-06 21:24:02 +0530

    Update basic_python.py

--- basic_python.py
+++ basic_python.py
@@ -66,4 +66,143 @@
 # elif(a>20 and a<=24):
 #     print("good night")    
 # else:
-#     print("vaibhav mota hai")          
+#     print("vaibhav mota hai")     
+# # a={1,2,3,}
+# # b={4,5,6}
+
+# # import time
+# # timestamp=time.strftime('%H:%M:%S')
+# # print(timestamp)
+# # timestamp=time.strftime('%H')
+# # print(timestamp)
+# # timestamp=time.strftime('%M')
+# # print(timestamp)
+# # timestamp=time.strftime('%S')
+# # print(timestamp)
+# #DAY 16
+# # x=int("khushi")
+
+# # match x:
+# #     case 0:
+# #         print("thora sa takla")
+# #     case 1:
+# #         print("thora jada takla" )  
+# #     case _:
+# #         print("pura takla") 
+
+# #DAY 17
+# # x=("khushi")
+
+# # for i in x:
+# #     print(i)
+# # for x in x:
+# #     print(x,end =",")    
+
+
+# # abhi=("aloo","bindi","began")
+
+# # for abhi in abhi:
+# #     print(abhi)
+# #     for abhi in abhi:
+# #         print(abhi)
+
+# # for i in range(5):
+# #     print(i)
+# # for i in range(1,12,2):
+# #     print(i)    
+# #DAY 18
+# # i=0
+# # while(i<3):
+# #     print("pagal")
+# #     i=i+1
+# #DAY 19
+# # for i in range(12):
+# #     print("5 X",i,"=",i*5)
+# #     if(i==10):
+# #         break
+# # for i in range(12):
+
+# #     if(i==10):
+# #         continue
+# #     print("5 X",i,"=",i*5)
+
+#  # DAY20
+# # def isgreater(a,b):
+# #     if a>b:
+# #         print("a is greater than b")
+
+# #     else:
+# #         print("b is greater than a")  
+
+# # isgreater(12,14)  
+
+# # def issmaller(a,b):
+# #     if a<b:
+# #         print("a is smaller than b")
+# #     else:
+# #         print("b is smaller than a")
+# # issmaller(12,19)
+
+# #DAY 21    
+
+# # def average(a,b,c,d):
+# #     i=(a+b+c+d)//2
+# #     print(i,"is average value")
+# # average(2,3,5,6)  
+
+# # def table(c):
+# #     for i in range(c):
+# #         print("5 X",i+1,"=",5*(i+1))
+# #         i=i+1
+# # table(10)
+# # def name(fname,mname,lname="ghose"):
+# #     print("hello ",fname,mname,lname)
+# # name("adi","suman")    
+
+# #DAY 22(list)
+# marks=[23,45,56,67,98,78,90,87,"andry",True]
+# print(marks)
+# print(marks[:])
+# print(marks[1:9:2])
+# print(marks[-1])
+# if 23 in marks:
+#     print("pagal")
+# else:
+#     print("seee")
+
+#DAY 23(list)
+# l=[1,7,3,5,9]
+# l.append(4)
+# print(l.count(1))
+# l.sort()
+# l.sort(reverse=True)
+# l.insert(3,67)
+# n=[12,13,14,15,19]
+#l.extend(n)
+# l.copy(n)
+# print(l.index(7))
+
+
+# DAY 24(tuple)
+
+# tup=(1,5,2,7)
+# print(type(tup),tup)
+# print(len(tup))
+# print(tup[1:4])
+
+# DAY 25(TUPLE methods)
+# tup=(1,2,34,5,6,3,3)
+# print(tup.count(3))
+
+
+
+
+
+
+
+
+
+
+
+
+
